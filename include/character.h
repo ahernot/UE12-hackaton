144c146d271208518e9342074d983610d2c58390
ahernot <70817470+ahernot@users.noreply.github.com>
2021-01-29 10:31:23 +0100

    initial commit

---
+++ include/character.h
@@ -0,0 +1,20 @@
+
+class Character {
+
+    public:
+        Character (const int& _type, const int& x, const int& y);
+
+    protected:
+
+        void _move (const int& x, const int& y);
+
+        int _xPos;
+        int _yPos;
+
+        int _type;
+
+
+        
+
+
+}
---
+++ src/character.cpp
@@ -0,0 +1,6 @@
+#include <string>
+#include "character.h"
+
+Character::Character (const int& type, const int& x, const int& y) _type(type), _xPos(x), _yPos(y) {
+
+}
