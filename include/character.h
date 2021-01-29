af22d038ae328fd2aa7795b505ff924995d76eb7
ahernot <70817470+ahernot@users.noreply.github.com>
2021-01-29 13:40:32 +0100

    update methods

--- include/character.h
+++ include/character.h
@@ -1,14 +1,17 @@
 #include <array>
+#include <vector>
 
 class Item {};
 class ArmorItem {
     public:
-        int getDefensePoints() {return 0;};
-        int getType() {return 0;};
+        int getDefensePoints () {return 0;};
+        int getType () {return 0;};
+        int getDurability () {return 10;};
 };
 class WeaponItem {
     public:
-        int getAttackPoints() {return 0;};
+        int getAttackPoints () {return 0;};
+        int getDurability () {return 10;};
 };
 
 
@@ -28,34 +31,43 @@ class Character {
         int getArmorVal (); // get armor protection value
         ArmorItem* replaceArmor (ArmorItem* newArmor); // outputs the previous armor item
         ArmorItem* getArmorItem (const int& pos);
+        void clearArmor ();
+        void clearBrokenArmor ();
 
         // Weapon management
         int getWeaponVal (); // get weapon damage value
         WeaponItem* replaceWeapon (WeaponItem* newWeapon); // outputs the previous weapon item
         WeaponItem* getWeaponItem ();
+        void clearWeapon ();
+        void clearBrokenWeapon ();
 
         // Health management
         int getHealth (); // return character health
         int addHealth (const int& healthToAdd);
         int removeHealth (const int& healthToRemove);
+        void clearHealth ();
 
         // Inventory management
-        std::array<Item*, 9> getInventory(); // return whole inventory // NO WORKO (SIZE = this->_inventorySlots)
+        std::vector<Item*> getInventory(); // return whole inventory
         Item* getInventory (const int& pos); // return specific item from inventory
         Item* replaceInventory (Item* newItem); // englobe tous les types d'Item ?
         Item* replaceInventory (Item* newItem, const int& pos);
         void removeInInventory (const int& pos); // delete if broken, with isBroken method from Item
+        void clearInventory ();
+        void clearBrokenInInventory();
+
+        void clearAllInventories ();
 
         // Global loop functions
         void cleanBroken (); // remove broken items in inventory, weapon slot, and armor slots
-        //void move (const int& x, const int& y); // move character
         void updatePosition (const int& x, const int& y); // update character position
+        void updateStatus (); // alive or dead
 
 
     protected:
 
-        // bool isDead; ???
-        int _inventorySlots; // remove
+        bool _isAlive = true; // to define elsewhere rather
+        int _inventorySlots;
 
         // Character type
         int _type;
@@ -74,6 +86,6 @@ class Character {
         WeaponItem* _weapon;
 
         // Inventory
-        std::array<Item*, 9> _inventory; // NO WORKO (SIZE = this->_inventorySlots)
+        std::vector<Item*> _inventory; // array
 
 }
--- src/character.cpp
+++ src/character.cpp
@@ -1,7 +1,6 @@
-#include <string>
 #include "Character.h"
 
-Character::Character (const int& type, const int& x, const int& y) _type(type), _xPos(x), _yPos(y) {  
+Character::Character (const int& type, const int& x, const int& y, const int& health, const int& inventorySlots): _type(type), _xPos(x), _yPos(y), _health(health), _inventorySlots(inventorySlots) {  
 };
 
 
@@ -48,7 +47,20 @@ ArmorItem* Character::getArmorItem (const int& pos) {
     return (this->_armor)[pos];
 };
 
+void Character::clearArmor () {
+    for (int i = 0; i < 4; i ++) {
+        (this->_armor)[i] = nullptr;
+    };
+};
 
+void Character::clearBrokenArmor () {
+    for (int i = 0; i < 4; i ++) {
+        ArmorItem* armorItem = (this->_armor)[i];
+        if (armorItem->getDurability() <= 0) {
+            (this->_armor)[i] = nullptr; // delete pointer (DELETE OBJECT???)
+        };
+    };
+};
 
 
 
@@ -81,6 +93,19 @@ WeaponItem* Character::getWeaponItem () {
     return this->_weapon;
 };
 
+void Character::clearWeapon () {
+    this->_weapon = nullptr;
+};
+
+void Character::clearBrokenWeapon () {
+    if ((this->_weapon)->getDurability() <= 0) {
+        this->_weapon = nullptr; // delete pointer (DELETE OBJECT???)
+    };
+};
+
+
+
+
 
 
 // Health management
@@ -96,10 +121,17 @@ int Character::removeHealth (const int& healthToRemove) {
     this->_health -= healthToRemove;
 };
 
+ void Character::clearHealth () {
+     this->_health = 0;
+ };
+
+
+
+
 
 
 // Inventory management
-std::array<Item*, 9> Character::getInventory () { // NO WORKO (SIZE = this->_inventorySlots)
+std::vector<Item*> Character::getInventory () {
     return this->_inventory;
 };
 
@@ -110,31 +142,57 @@ Item* Character::getInventory (const int& pos) {
 /**
  * Default method to add to end of inventory (englobe tous les types d'Item ?)
  */
-Item* Character::replaceInventory (Item* newItem); {
-    Item* oldItem = (this->_inventory)[8]; // Get last item of inventory
-    (this->_inventory)[8] = newItem;
-
+Item* Character::replaceInventory (Item* newItem) {
+    return Character::replaceInventory(newItem, 8); // Replace last item of inventory
 };
 
-Item* Character::replaceInventory (Item* newItem, const int& pos);
-void Character::removeInInventory (const int& pos); // delete if broken, with isBroken method from Item
+Item* Character::replaceInventory (Item* newItem, const int& pos) {
+    Item* oldItem = (this->_inventory)[pos];
+    (this->_inventory)[pos] = newItem;
+    return oldItem;
+};
+
+void Character::removeInInventory (const int& pos) {
+    (this->_inventory)[pos] = nullptr;
+}; // delete if broken, with isBroken method from Item
+
+
+
+void Character::cleanBroken () {
+    this->clearBrokenArmor ();
+    this->clearBrokenWeapon ();
+    // Clean broken items in inventory // TODO
+};
 
 
 
 
+    void Character::clearAllInventories () {
+        this->clearArmor();
+        this->clearWeapon();
+        this->clearInventory();
+    };
 
-/*
-void Character::move (const int& x, const int& y) {
-    this->_xPos += x;
-    this->_yPos += y;
-}
-*/
 
 
 void Character::updatePosition (const int& x, const int& y) {
     this->_xPos = x;
     this->_yPos = y;
 }
+
+
+void Character::updateStatus () {
+    if (this->_health <= 0) {
+        this->_isAlive = false;
+    };
+};
+
+
+
+
+
+
+
 
 // #inventory -> calculate minecraft armor, 1 weapon
 // #health
